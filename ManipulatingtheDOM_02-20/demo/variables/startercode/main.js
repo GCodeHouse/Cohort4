95d4d6d9fec747b97fa353f847cf5582636353b1
Rizel Scarlett <blackgirlbytes@github.com>
2022-02-19 15:03:17 -0500

    Create main.js

---
+++ ManipulatingtheDOM_02-20/demo/variables/startercode/main.js
@@ -0,0 +1,15 @@
+// people use console logs to help them check if their javascript is working
+// console logs print out in the console of your browser
+// lets check to see if the words hello world appear in a browser
+// if it does, that means our javascript is connected to our html
+
+console.log('hello world')
+
+// lets try to get the body element by its id
+
+document.getElementById('main-background')
+                        
+// althought that syntax is right, we don't know if it worked,
+// so let's console log it to see that it grabbed the right element 
+
+console.log(document.getElementById('main-background'))
