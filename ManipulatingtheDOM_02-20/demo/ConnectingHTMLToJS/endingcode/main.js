c8d5d4f8a97206627607adde821258652d932c18
Rizel Scarlett <blackgirlbytes@github.com>
2022-02-19 14:43:19 -0500

    Create main.js

---
+++ ManipulatingtheDOM_02-20/demo/ConnectingHTMLToJS/endingcode/main.js
@@ -0,0 +1,5 @@
+// people use console logs to help them check if their javascript is working
+// console logs print out in the console of your browser
+// lets check to see if the words hello world appear in a browser
+// if it does, that means our javascript is connected to our html
+console.log('hello world')
