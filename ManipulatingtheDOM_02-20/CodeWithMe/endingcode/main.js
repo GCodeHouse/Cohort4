63639726db64387506a9decc8c70ce03caf84022
Rizel Scarlett <blackgirlbytes@github.com>
2022-02-19 17:35:11 -0500

    Update main.js

--- ManipulatingtheDOM_02-20/CodeWithMe/endingcode/main.js
+++ ManipulatingtheDOM_02-20/CodeWithMe/endingcode/main.js
@@ -1,8 +1,18 @@
 function toggleDarkMode() {
-  var bodyElement =document.getElementById("mainBackground")
-  	  if ( bodyElement.style.background == "black") {
-        bodyElement.style.background = "thistle"
+    var bodyElement = document.getElementById("mainBackground")
+    var darkModeBtnElement = document.getElementById("darkModeButton")
+    console.log(darkModeBtnElement);
+    // if it's in dark mode, change it to light mode
+    // what does dark mode look like here?
+    // dark mode looks like a black background and the button will say Change to Light Mode
+    if (bodyElement.style.background == "black") {
+        bodyElement.style.background = "#F9EAE1"
+        darkModeBtnElement.innerHTML="Change to Light Mode"
+   // if it's in light mode, change it to dark mode
+    // what does light mode look like here?
+    // light mode looks like a tan background and the button will say Change to Dark Mode
     } else {
-      bodyElement.style.background  = "black"
+        bodyElement.style.background = "black"
+        darkModeBtnElement.innerHTML="Change to Dark Mode"
     }
 }
